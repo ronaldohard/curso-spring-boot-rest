fdc05c0d8231ec8092c4370f8ef29342fb09381a
Ronaldo Prates <ronaldoprates@iMac-de-Ronaldo.local>
2018-04-19 21:12:03 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/ronaldoprates/cursos/CursoSpringBootRestApplication.java
@@ -0,0 +1,13 @@
+package com.ronaldoprates.cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBootRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBootRestApplication.class, args);
+		System.out.println("------------------kansdkasndkasd------------------");
+	}
+}
---
+++ src/test/java/com/ronaldoprates/cursos/CursoSpringBootRestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ronaldoprates.cursos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursoSpringBootRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
